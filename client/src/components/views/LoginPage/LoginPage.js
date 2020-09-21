a10602e168336992183486571fc38478db18d03a
kmkim <kmkim@triphos.co.kr>
2020-09-22 00:15:56 +0900

    로그인, 로그아웃, 회원가입

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,16 +1,62 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import Axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_action/user_action';
 
-const LoginPage = props => {
+function LoginPage (props) {
+    const dispatch = useDispatch();
+    const [Email, setEmail] = useState("");
+    const [Password, setPassword] = useState("");
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        let body = {
+            email: Email, 
+            password: Password, 
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+                if(response.payload.loginSuccess) {
+                    props.history.push('/')         // root-page 화면이동
+                }
+                else {
+                    alert('err');
+                }
+            })
+
+
+    }
+
     return (
-        <div>
-            LoginPage
-        </div>
-    );
-};
+        <div style={{display:'flex', justifyContent:'center', alignItems:'center'
+            , width: '100%', height: '100vh' }}
+            onSubmit={onSubmitHandler}
+        >
 
-LoginPage.propTypes = {
-    
-};
+            <form style={{display:'flex', flexDirection:'column'}}>
+                <label>Email</label>
+                <input type="email" value={ Email } onChange={ onEmailHandler } />
+               
+                <label>Password</label>
+                <input type="password" value={ Password }  onChange={ onPasswordHandler } />
+                
+                <br/>
+                <button>
+                    Login
+                </button>
+            </form>
+        </div>
+    )
+}
 
 export default LoginPage;
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from 'redux-promise'
+import ReduxThunk from 'redux-thunk'
+import Reducer from './_reducers'
 
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
-import 'antd/dist/antd.css'; 
-
-ReactDOM.render(<App />,  document.getElementById('root'));
+ReactDOM.render(
+    <Provider
+        store={createStoreWithMiddleware(Reducer,
+            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+            )}
+    >
+         <App/>
+    </Provider>
+    ,  document.getElementById('root'));
 
 serviceWorker.unregister();
